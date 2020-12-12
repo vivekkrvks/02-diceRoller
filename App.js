8edf8ad94a372cc46af52a5cb171079d0ed09770
Kumar Vivek -X (kumvive3 - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kumvive3@partnet.cisco.com>
2020-12-12 17:46:49 +0530

    all design part completed

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
+  Image,
   StyleSheet,
   ScrollView,
   View,
@@ -18,14 +19,45 @@ import DiceFive from './assets/dice5.png'
 import DiceSix from './assets/dice6.png'
 
 const App = () => {
+  const [uri, setUri] = useState(DiceOne)
+
   return(
     <>
-    <View>
+    <View style = {styles.container}>
+      <Image style = {styles.image} source={uri}></Image>
       <TouchableOpacity>
-    <Text>Vivek</Text></TouchableOpacity>
+    <Text style = {styles.pButton}>Play Game</Text></TouchableOpacity>
     </View>
     </>
   )
 }
 
 export default App
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    backgroundColor:'#222831',
+    alignItems:"center", // right to left
+    justifyContent:"center"
+  },
+  image:{
+    height:300,
+    width:300,
+    marginTop:10,
+    marginBottom:50
+  },
+
+
+  pButton: {
+    fontSize:20,
+    color:"#F2A365",
+    fontWeight:"bold",
+    borderColor:"#30475E",
+    borderWidth:3,
+    borderRadius:5,
+    paddingHorizontal:40,
+    paddingVertical:10
+  }
+})
