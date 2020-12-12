8cbbc8328ccedbc34167900f7fae2a7758d6561d
Kumar Vivek -X (kumvive3 - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kumvive3@partnet.cisco.com>
2020-12-12 17:20:24 +0530

    just basic boiler plate code

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import DiceOne from './assets/dice1.png'
+import DiceTwo from './assets/dice2.png'
+import DiceThree from './assets/dice3.png'
+import DiceFour from './assets/dice4.png'
+import DiceFive from './assets/dice5.png'
+import DiceSix from './assets/dice6.png'
+
+const App = () => {
+  return(
+    <>
+    <View>
+      <TouchableOpacity>
+    <Text>Vivek</Text></TouchableOpacity>
+    </View>
+    </>
+  )
+}
+
+export default App
